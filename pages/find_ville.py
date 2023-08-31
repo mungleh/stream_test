9fa262520a159ee9af797fad21013a4a03de6357
MMailleur <104862908+MMailleur@users.noreply.github.com>
2023-08-31 14:43:11 +0200

    Update find_ville.py

--- pages/find_ville.py
+++ pages/find_ville.py
@@ -4,17 +4,17 @@ import streamlit.components.v1 as components
 import pydeck as pdk
 import os
 
-# Mise en forme CSS pour la page
+# Configuration des pages
 st.set_page_config(
     page_title="Page principale",
     layout="wide"
 )
 
-# Clé API d'OpenWeather
+# Clé API d'OpenWeather qui le streamlit deployer a dans ses secrets
 key = st.secrets["API_KEY"]
 
 
-# Fonction pour obtenir la localisation d'une ville
+# Fonction pour obtenir la localisation d'une ville , elle retourne un json 
 @st.cache_data
 def get_ville_loc(ville):
     url = f"https://api.openweathermap.org/geo/1.0/direct?q={ville}&appid={key}"
@@ -47,7 +47,7 @@ col1, col2 = st.columns(2)
 # Bouton pour prédire
 with col2:
     predict = st.button("Prédire")
-
+    # si la ville existe on requete l'api pour la temperature et ses coordonnées
     if predict:
         try :
             # Obtention de la localisation de la ville
@@ -57,13 +57,16 @@ with col2:
             # Requête pour obtenir les informations météo de la ville
             info_meteo = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={key}').json()
             temp = info_meteo['main']['temp']
+        #dans le cas ou l'api ne renvoie pas de resultat on affiche une erreur
         except :
             st.markdown("Ville introuvable ou erreur de saisie")
 # Affichage de la température
 with col1:
     try:
+        # affichage de la température
         st.markdown(f'<p style="font-family:sans-serif; font-size: 42px;">{temp}°C</p>', unsafe_allow_html=True)
     except:
+        # Affichage par défaut si il n'y a pas de pred a changer
         st.markdown(f'En attente de prédiction')
 
 # Affichage de la carte
